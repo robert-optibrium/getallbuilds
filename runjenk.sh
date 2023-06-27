79c58cb1a54417d6125910266280297f69e7917c
robert-optibrium <118751337+robert-optibrium@users.noreply.github.com>
2023-06-27 13:30:21 +0100

    one more time

--- runjenk.sh
+++ runjenk.sh
@@ -2,6 +2,7 @@
 set -x
 echo Creating VENV
 python3 -m venv venv
+ls -lr
 source venv/bin/activate
 pip install -r requirements.txt
 python main.py -gab -gpd -nod -geb -usr robert@optibrium.com -tok 11bf54d1a2a09cf5c2b52971ec0d848568
